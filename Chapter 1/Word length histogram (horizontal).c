45a49b14fa4efc9f1a896158e009a12787191029
anasrazakhan <raza.anass@gmail.com>
2018-10-04 01:20:25 -0900

    Updated Exercise 1-13

--- Chapter 1/Word length histogram (horizontal).c
+++ Chapter 1/Word length histogram (horizontal).c
@@ -1,22 +1,46 @@
 #include <stdio.h> 
 int main(){
 	double c;
-	int length[10], i=0;
-	for(int k = 0; k < 10; k++)
-	length[k] = 0;
+	int nalph[26], i=0, wlength[20], wcount = 0, lcount = 0, frequency[20], x = 0;
+	for(int k = 0; k < 20; k++)
+	wlength[k] = 0;
+	for(int k = 0; k < 20; k++)
+	frequency[k] = 0;
+	for(int k = 0; k < 26; k++)
+	nalph[k] = 0;
 	while((c = getchar()) != EOF){
-	//	i = 0;
-		if(c != ' ' && c != '\n' && c != 9) //9 is the ASCII code for tab
-		{
-			i = i + 1;
-			length[0] = i;
-			printf("%d ",length[0]);
-		}
-		else{
+		if( c >= 'A' && c <= 'Z' ){
+			int d = c - 'A';
+			nalph[d] = nalph[d] + 1;
+			lcount++;
+			}
+		else if( c == ' ' || c == '\n' || c == '\t'){
+			wcount++;
+			wlength[wcount] = lcount;
+			lcount = 0;
+			}
+	}
+	printf("\nalphabets = ");
+	for (int i = 0; i < 26 ; i++)
+		printf(" %d", nalph[i]);
+	printf("\n %d",wcount);
+	printf("\n f =");
+	for (int i =1; i <= 20 ; i++)
+	printf(" %d",wlength[i]);
+	printf("\n fr =");
+	for (int i = 0; i < 20 ; i++){
+	x = wlength[i];
+	frequency[x] = frequency[x] + 1;
+	}
+	for (int i = 1; i <=20; i++)
+	printf(" %d",frequency[i]);
+	printf("\n \n");
+	for(int i = 1; i <= 20; i++){
+		printf("%d ",i);
+		for(int k = 0; k < frequency[i]; k++)
+		printf("-");
 		printf("\n");
-		i = 0;
-		}
-
 	}
-	}
+	
+}
 
