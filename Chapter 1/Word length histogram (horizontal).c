c902f1ed6ff4043808429d626fd47765c808578c
anasrazakhan <raza.anass@gmail.com>
2018-10-03 23:03:27 -0900

    Updated Exercise 1-13

--- Chapter 1/Word length histogram (horizontal).c
+++ Chapter 1/Word length histogram (horizontal).c
@@ -1,20 +1,22 @@
 #include <stdio.h> 
 int main(){
 	double c;
-	int length[10], i;
+	int length[10], i=0;
 	for(int k = 0; k < 10; k++)
 	length[k] = 0;
 	while((c = getchar()) != EOF){
-		i = 0;
+	//	i = 0;
 		if(c != ' ' && c != '\n' && c != 9) //9 is the ASCII code for tab
 		{
 			i = i + 1;
-			for(int n = 0; n < i; n++){
-			length[n] = n;
-			printf("%d ",length[n]);
+			length[0] = i;
+			printf("%d ",length[0]);
 		}
+		else{
+		printf("\n");
+		i = 0;
 		}
-		else
-			printf("\n");
+
 	}
-}
+	}
+
